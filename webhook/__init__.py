0daac44f7680e84273d20046bcc92c5155fe5167
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-03-15 10:09:02 -0700

    check for not empty and none for category

--- webhook/__init__.py
+++ webhook/__init__.py
@@ -85,12 +85,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                                 elif submission_json["IDA_SUBMISSION_STATUS_id"] \
                                     == statuses["RECORDED"] \
-                                        and "IDA_CATEGORY_ID_LOOKUP_id" in submission_json:
+                                        and "IDA_CATEGORY_ID_LOOKUP_id" in submission_json \
+                                            and submission_json["IDA_CATEGORY_ID_LOOKUP_id"] \
+                                                is not None:
                                     print("Record Category ID:" + \
                                         str(record_json["IDA_CATEGORY_ID"]))
                                     print("Submission Category ID:" + \
                                         str(submission_json["IDA_CATEGORY_ID_LOOKUP_id"]))
-                                    if "IDA_CATEGORY_ID" in record_json:
+                                    if "IDA_CATEGORY_ID" in record_json and \
+                                        record_json["IDA_CATEGORY_ID"] is not None:
                                         if record_json["IDA_CATEGORY_ID"] != \
                                             submission_json["IDA_CATEGORY_ID_LOOKUP_id"]:
 
